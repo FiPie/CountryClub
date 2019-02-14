9fbea720ec8023284238b8bdf1aa9c4c742b0ebf
Filip Pie <filipiusz@gmail.com>
2019-02-14 12:11:59 +0100

    optymalizacje drobne kodu

--- src/main/java/com/f/piechowiak/spring/countryclub/repositories/MessageRepository.java
+++ src/main/java/com/f/piechowiak/spring/countryclub/repositories/MessageRepository.java
@@ -4,9 +4,12 @@ import com.f.piechowiak.spring.countryclub.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
 
-
+    List<Message> findAllByReceiverId(Long memberId);
+    List<Message> findAllBySenderId(Long memberId);
 }
